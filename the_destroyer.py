61c450bfc2f290e9768b08ea51d1f44724754890
SOORAJTS2001 <cherrycheeks404@gmail.com>
2023-04-13 14:27:12 +0530

    corrected annex 5 issue,optimized code with reducin loop for width and height

--- the_destroyer.py
+++ the_destroyer.py
@@ -5,8 +5,10 @@ while True:
     for image_name in os.listdir(images_path):
         current_time = datetime.datetime.now().strftime("%H-%M")
         image_time = image_name.split('.')[0]
+        print(current_time, image_time)
         if current_time >= image_time:
             image_path = os.path.join(images_path, image_name)
             print("deleted: " + image_time+".jpg")
+            print("waiting for 5 minutes")
             os.remove(image_path)
             time.sleep(300)
