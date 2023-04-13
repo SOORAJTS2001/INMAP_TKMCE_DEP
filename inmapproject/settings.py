de993f73008ee26d6b18491ff103c6affffd6ee9
SOORAJTS2001 <cherrycheeks404@gmail.com>
2023-04-13 15:21:12 +0530

    removed the glitch in the worker

--- inmapproject/settings.py
+++ inmapproject/settings.py
@@ -128,6 +128,9 @@ STATIC_URL = '/static/'
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'static/inmapapp'),
+    os.path.join(BASE_DIR, 'static/inmapapp/temp_images'),
+    
 )
 
 STATIC_URL = 'static/'
