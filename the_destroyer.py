0ceb091b28ad71382ce48b3740ad49b36708b02b
SOORAJTS2001 <cherrycheeks404@gmail.com>
2023-04-13 14:40:53 +0530

    removed the glitch in the worker

--- the_destroyer.py
+++ the_destroyer.py
@@ -1,16 +1,17 @@
 import os
 import datetime,time
 directory = os.getcwd() 
-print(directory)
-images_path = 'inmapapp/static/inmapapp/temp_images/'
+images_path = os.path.abspath(f'inmapapp/static/inmapapp/temp_images/')
+# print(images_path)
 while True:
     for image_name in os.listdir(images_path):
+        print(image_name)
         current_time = datetime.datetime.now().strftime("%H-%M")
         image_time = image_name.split('.')[0]
-        print(current_time, image_time)
+        # print(current_time, image_time)
         if current_time >= image_time:
             image_path = os.path.join(images_path, image_name)
             print("deleted: " + image_time+".jpg")
-            print("waiting for 5 minutes")
+            # print("waiting for 5 minutes")
             os.remove(image_path)
-            time.sleep(300)
+    time.sleep(60)
