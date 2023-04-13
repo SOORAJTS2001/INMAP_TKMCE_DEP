6f42b740d71dad296edf182d8d2243849beb3ed6
SOORAJTS2001 <cherrycheeks404@gmail.com>
2023-04-13 17:03:22 +0530

    removed the glitch in the worker

--- the_destroyer.py
+++ the_destroyer.py
@@ -1,19 +1,22 @@
 import os
-import datetime,time
-directory = os.getcwd() 
-images_path = os.path.abspath(f'staticfiles/inmapapp/temp_images')
-# print(images_path)
-# Create the directory if it does not exist
+import datetime
+import time
+import pytz
+
+images_path = os.path.abspath('staticfiles/inmapapp/temp_images')
+
+# Set the time zone to Asia/Kolkata
+tz = pytz.timezone('Asia/Kolkata')
+
+# Loop indefinitely
 while True:
     for image_name in os.listdir(images_path):
         print(image_name)
-        current_time = datetime.datetime.now().strftime("%H-%M")
+        current_time = datetime.datetime.now(tz).strftime("%H-%M")
         image_time = image_name.split('.')[0]
-        print("server time is: ",current_time)
-        # print(current_time, image_time)
+        print("server time is: ", current_time)
         if current_time >= image_time:
             image_path = os.path.join(images_path, image_name)
-            print("deleted: " + image_time+".jpg")
-            # print("waiting for 5 minutes")
+            print("deleted: " + image_time + ".jpg")
             os.remove(image_path)
     time.sleep(60)
