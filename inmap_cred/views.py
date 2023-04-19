db618b8bde49721820e1d67eb1a165a75ef0624b
SOORAJTS2001 <cherrycheeks404@gmail.com>
2023-04-19 16:57:50 +0530

    unchanged the stuff

--- inmap_cred/views.py
+++ inmap_cred/views.py
@@ -18,4 +18,19 @@ def create_account(request):
     else:
         # If the request method is not POST, just render the form template
         return render(request,'inmap_cred/create_account.html')
+def change_account_api(request):
+    if request.method == 'POST':
+        # Retrieve the form data from the request object
+        organization = request.POST['organization']
+        institution = request.POST['institution']
+        expected_requests = request.POST['expected-requests']
+        api_cred = request.POST['api_cred']
+        api_secret = request.POST['api_secret']
+        apiaccounts = ApiAccounts.objects.create(organization=organization,institution=institution,api_cred=api_cred,api_secret=api_secret,expected_requests=expected_requests)
+        apiaccounts.save()
+        print("created account of api cred {api_cred} and api secret {api_secret}")
     
+        return render(request,'inmap_cred/create_account.html',{'api_cred':api_cred,'api_secret':api_secret})
+    else:
+        # If the request method is not POST, just render the form template
+        return render(request,'inmap_cred/create_account.html')
